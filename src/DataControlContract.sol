465b137246b3c78c2b6b2c3142a15d22300ecb75
Shankar Subramanian <shankars0899@gmail.com>
2022-05-12 05:05:57 +0000

    added Contract Funding

--- src/DataControlContract.sol
+++ src/DataControlContract.sol
@@ -20,27 +20,40 @@ contract StorkDataControlContract {
     struct StorkNode {
         uint256 stakeValue;
         uint256 stakeEndTime;
-        uint32 txCount;
+        uint256 txCount;
         bool isActive;
+    }
+
+    struct StorkContract {
+        uint256 maxTxCount;
+        uint256 txCount;
     }
 
     /// @notice Minimum Stake amount used to activate a validator
     /// @dev
 
     uint256 private minStake;
+    uint256 private costPerTx;
+
     address public immutable multiSigWallet;
 
     uint256 public constant stakeHours = 86400;
     uint256 public constant stakeDays = 30;
 
     mapping(address => StorkNode) public storkNodes;
+    mapping(address => StorkContract) public storkContracts;
 
-    constructor(uint256 _minStake, address _multiSigWallet) {
+    constructor(
+        uint256 _minStake,
+        uint256 _costPerTx,
+        address _multiSigWallet
+    ) {
         minStake = _minStake;
+        costPerTx = _costPerTx;
         multiSigWallet = _multiSigWallet;
     }
 
-    function addValidator() public payable {
+    function addStorkNode() public payable {
         require(msg.value > minStake, "Deposit must be greater than 0");
 
         storkNodes[msg.sender] = StorkNode(
@@ -51,8 +64,8 @@ contract StorkDataControlContract {
         );
     }
 
-    function increaseStake() public payable {
-        require(msg.value > minStake, "Deposit must be greater than 0");
+    function fundStorkNodeStake() public payable {
+        require(msg.value > minStake, "Stake must be greater than 0");
 
         storkNodes[msg.sender].stakeValue += msg.value;
         storkNodes[msg.sender].stakeEndTime +=
@@ -60,9 +73,35 @@ contract StorkDataControlContract {
             (stakeHours * stakeDays);
     }
 
-    function increaseTxCount(address[] calldata txAddrs) public onlyMultiSigWallet {
-        for(uint i = 0; i < txAddrs.length; ++i) {
-            storkNodes[txAddrs[i]].txCount++;
+    function storkNodeTxController(address[] calldata txStorkAddrs)
+        public
+        onlyMultiSigWallet
+    {
+        for (uint256 i = 0; i < txStorkAddrs.length; ++i) {
+            storkNodes[txStorkAddrs[i]].txCount++;
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    function addStorkContract() public payable {
+        require(msg.value > minStake, "Funds must be greater than 0");
+
+        storkContracts[msg.sender] = StorkContract(msg.value / costPerTx, 0);
+    }
+
+    function fundStorkContractStake(address _storkContractAddr) public payable {
+        require(msg.value > minStake, "Funds must be greater than 0");
+
+        storkContracts[_storkContractAddr].maxTxCount += msg.value / costPerTx;
+    }
+
+    function contractTxController(address[] calldata txContractAddrs)
+        public
+        onlyMultiSigWallet
+    {
+        for (uint256 i = 0; i < txContractAddrs.length; ++i) {
+            storkContracts[txContractAddrs[i]].txCount++;
         }
     }
 }
