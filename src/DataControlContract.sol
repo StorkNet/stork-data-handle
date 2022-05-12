36a12a26f12e4d99c3929cfb04e26a4675599785
Shankar Subramanian <shankars0899@gmail.com>
2022-05-12 05:37:00 +0000

    added batching of jobs

--- src/DataControlContract.sol
+++ src/DataControlContract.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.10;
 
 /// @title StorkNet's OnChain Data Control Contract
 /// @author Shankar "theblushirtdude" Subramanian
-/// @notice 
+/// @notice
 /// @dev Explain to a developer any extra details
-contract StorkDataControlContract {
-
+contract DataControlContract {
     /// @dev Only validated users can access the function
     modifier OnlyStorkNodes() {
         require(storkNodes[msg.sender].isActive == true, "Not a validator");
@@ -30,7 +29,6 @@ contract StorkDataControlContract {
         uint256 maxTxCount;
         uint256 txCount;
     }
-
 
     uint256 private minStake;
     uint256 private costPerTx;
@@ -73,19 +71,19 @@ contract StorkDataControlContract {
             (stakeHours * stakeDays);
     }
 
-    function storkNodeTxController(address[] calldata txStorkAddrs)
-        public
-        onlyMultiSigWallet
-    {
+    function storkNodeTxController(
+        address[] calldata txStorkAddrs,
+        uint256[] calldata _txCounts
+    ) public onlyMultiSigWallet {
         for (uint256 i = 0; i < txStorkAddrs.length; ++i) {
-            storkNodes[txStorkAddrs[i]].txCount++;
+            storkNodes[txStorkAddrs[i]].txCount += _txCounts[i];
         }
     }
 
     // -----------------------------------------------------------------------------------------------------------------
 
     /// @notice A StorkContract is a contract that uses StorkNet to decouple data from the EVM contract
-    /// @dev On the creation of a StorkContract funds must be transferred that are used to compute the 
+    /// @dev On the creation of a StorkContract funds must be transferred that are used to compute the
     ///      total number of transactions that it can handle
     function addStorkContract() public payable {
         require(msg.value > minStake, "Funds must be greater than 0");
@@ -95,7 +93,7 @@ contract StorkDataControlContract {
 
     /// @notice Any user can further fund a StorkContract
     /// @dev Increase the funding of the StorkContract
-    /// @param _storkContractAddr a parameter that is used to pass the address of the stork contract 
+    /// @param _storkContractAddr a parameter that is used to pass the address of the stork contract
     ///         that is being funded
     function fundStorkContractStake(address _storkContractAddr) public payable {
         require(msg.value > minStake, "Funds must be greater than 0");
@@ -106,14 +104,14 @@ contract StorkDataControlContract {
     /// @notice Updates the number of data storing Txs that were involved with this StorkContract
     /// @dev This function is only executable by the StorkMultiSig wallet as we treat batches of Txs as a single
     ///      transaction on the main EVM chain
-    /// @param _txContractAddrs contains the list of StorkContract addresses that had any txs involving data change  
+    /// @param _txContractAddrs contains the list of StorkContract addresses that had any txs involving data change
     ///        on the StorkNet that were sent to them
-    function contractTxController(address[] calldata _txContractAddrs)
-        public
-        onlyMultiSigWallet
-    {
+    function contractTxController(
+        address[] calldata _txContractAddrs,
+        uint256[] calldata _txCounts
+    ) public onlyMultiSigWallet {
         for (uint256 i = 0; i < _txContractAddrs.length; ++i) {
-            storkContracts[_txContractAddrs[i]].txCount++;
+            storkContracts[_txContractAddrs[i]].txCount += _txCounts[i];
         }
     }
 }
