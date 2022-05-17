efbe692b47fbace715d638e64bcf14b057045336
Shankar Subramanian <shankars0899@gmail.com>
2022-05-17 04:27:25 +0000

    Removed redundant code

--- src/MultiSigVerification.sol
+++ src/MultiSigVerification.sol
@@ -232,5 +232,3 @@ contract MultiSigVerification {
         return (transactions[_txIndex]);
     }
 }
-
-// ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"]
