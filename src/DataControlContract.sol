2074069bf2c639b0c720b49f3eb333e701f3acf9
Shankar Subramanian <shankars0899@gmail.com>
2022-05-17 05:54:25 +0000

    Now using 4 weeks instead of computing time in seconds

--- src/DataControlContract.sol
+++ src/DataControlContract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 /// @title StorkNet's OnChain Data Control Contract
 /// @author Shankar "theblushirtdude" Subramanian
 /// @notice
-/// @dev Explain to a developer any extra details
+/// @dev This contract is used to manage the on-chain data of StorkContracts.
 contract DataControlContract {
     /// @dev Only validated users can access the function
     modifier OnlyStorkNodes() {
@@ -14,34 +14,47 @@ contract DataControlContract {
 
     /// @dev Only the multi sig wallet can access these functions that update batches so that we lower gas fees
     modifier onlyMultiSigWallet() {
-        require(msg.sender == multiSigWallet, "Not multi sig wallet");
+        require(msg.sender == multiSigVerifierContract, "Not multi sig wallet");
         _;
     }
-    
+
     /// @dev Stores data about the StorkNodes
+    /// @custom: amount staked,
+    /// @custom: the duration till when the StorkNode is active, after which it can get back it's stake
+    /// @custom: the number of transactions handled by the StorkNode
+    /// @custom: Whether or not this storknode is active to handle data requests
+
     struct StorkNode {
         uint256 stakeValue;
         uint256 stakeEndTime;
         uint256 txCount;
+        /// @custom: @shankars99 - Make function to unstakeif the StorkNode misbehaves and remove this bool
         bool isActive;
     }
 
+    /// @dev Stores data about the StorkContracts
+    /// @custom: number of transactions handled for the StorkContract,
+    /// @custom: the duration till when the StorkContract is active, after which it can get back it's stake
     struct StorkContract {
         uint256 txCount;
+        /// @custom: @shankars99 - Try making a function to unstake the stake if requested by the StorkContract
         bool isActive;
     }
 
-    /// @notice Explain to an end user what this does
-    /// @dev Explain to a developer any extra details
-    /// @return Documents the return variables of a contract’s function state variable
+    /// @notice The minimum stake required to be a StorkNode or StorkContract
+    /// @dev The stake is used to validate Validators and also to compensate/pay Validators for transactions they handle
     uint256 public minStake;
+
+    /// @notice The cost per transaction to be paid by the StorkContract
+    /// @dev Reduces the amount staked by the StorkContract
     uint256 public costPerTx;
-    uint256 public totalFunding;
 
-    address public immutable multiSigWallet;
+    /// @notice The cost per transaction to be paid by the StorkContract
+    /// @dev Reduces the amount staked by the StorkContract
+    address public immutable multiSigVerifierContract;
 
-    uint256 public constant stakeHours = 86400;
-    uint256 public constant stakeDays = 30;
+    /// @notice 1 hour in minutes
+    uint256 public constant stakeTime = 4 weeks;
 
     mapping(address => StorkNode) public storkNodes;
     mapping(address => StorkContract) public storkContracts;
@@ -49,11 +62,11 @@ contract DataControlContract {
     constructor(
         uint256 _minStake,
         uint256 _costPerTx,
-        address _multiSigWallet
+        address _multiSigVerifierContract
     ) {
         minStake = _minStake;
         costPerTx = _costPerTx;
-        multiSigWallet = _multiSigWallet;
+        multiSigVerifierContract = _multiSigVerifierContract;
     }
 
     function addStorkNode() external payable {
@@ -62,7 +75,7 @@ contract DataControlContract {
 
         storkNodes[msg.sender] = StorkNode(
             msg.value,
-            block.timestamp + (stakeHours * stakeDays),
+            block.timestamp + stakeTime,
             0,
             true
         );
@@ -74,9 +87,7 @@ contract DataControlContract {
         require(msg.value > minStake, "Stake must be greater than 0");
 
         storkNodes[msg.sender].stakeValue += msg.value;
-        storkNodes[msg.sender].stakeEndTime +=
-            block.timestamp +
-            (stakeHours * stakeDays);
+        storkNodes[msg.sender].stakeEndTime += block.timestamp + stakeTime;
 
         emit NodeStakeExtended(msg.sender, storkNodes[msg.sender].stakeEndTime);
     }
