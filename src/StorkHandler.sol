fc8a6d067846fefe97e69ec109babdb0242fe2de
Shankar Subramanian <shankars0899@gmail.com>
2022-05-17 18:53:41 +0000

    added create, update, delete

--- src/StorkHandler.sol
+++ src/StorkHandler.sol
@@ -16,20 +16,41 @@ contract StorkHandler {
     /// @custom: Solidity data type (string, uint256, bool, etc) of the variable
     /// @custom: Variable of the data type (name, age, isMale, etc)
     /// @custom: The index of the variable for arrays or mappings
-    struct StorkDataType {
+    struct StorkType {
         string varType;
         string varName;
         string varIndex;
     }
 
+    /// @notice Storks are the smallest data unit of the StorkNet
+    /// @dev It contains the data stored in the request along with a unique id wrt the collection
+    /// @custom: Uniquely identifiable id for each data stored
+    /// @custom: Variable of the data type (name, age, isMale, etc)
+    /// @custom: Variable of the data type (name, age, isMale, etc)
+    struct Stork {
+        uint32 _id;
+        bytes _data;
+    }
+    
+    /// @notice A collection of Storks
+    /// @dev Contains info on a particular group of storks 
+    /// @custom: The size of the Phalanx (the collection of storks)
+    /// @custom: The stork type id for lookups 
+    /// @custom: Name proposed by Srinidhi 
+    struct Phalanx {
+        uint32 storkLastId;
+        uint32 storkTypeId;
+    }
+
     /// @notice Associates a id number with your custom storkDataType
-    /// @dev Converts the datatype name to the datatype id
-    /// @return The id for the datatype
-    mapping(string => uint256) public dataType;
+    /// @dev Maps the data type name to a StorkDataType object
+    mapping(string => Phalanx) public phalanxInfo;
 
     /// @notice Counts the number of storkDataTypes
     /// @dev Used to keep track of the number of storkDataTypes
-    uint256 public dataTypeCount;
+    uint32 public storkTypeCount;
+
+    mapping(string => bool) phalanxExists;
 
     /// @notice Sets the address of the DCC
     /// @dev If the address is not set, then set the address of the DCC
@@ -68,7 +89,7 @@ contract StorkHandler {
     /// @notice Converts a StorkDataType to a bytes array for easier use as a parameter/event value
     /// @dev A bytes version is preferable as it's easier to handle
     /// @param _data a parameter just like in doxygen (must be followed by parameter name)
-    function encodeTypes(StorkDataType[] calldata _data)
+    function encodeTypes(StorkType[] calldata _data)
         public
         pure
         returns (bytes memory)
@@ -83,54 +104,93 @@ contract StorkHandler {
     function decodeTypes(bytes calldata _data)
         public
         pure
-        returns (StorkDataType[] memory)
+        returns (StorkType[] memory)
     {
-        return (abi.decode(_data, (StorkDataType[])));
+        return (abi.decode(_data, (StorkType[])));
     }
 
     /// @notice Creates a new StorkDataType based on the parameters given
     /// @dev Links the StorkDataType with unique name and id, then emits an event for off-chain processing
-    /// @param _name The name of the StorkDataType
-    /// @param _data The bytes version of the StorkDataType
-    function createNewType(string calldata _name, bytes calldata _data)
+    /// @param _storkName The name of the StorkDataType
+    /// @param _storkData The bytes version of the StorkDataType
+    function createNewType(string calldata _storkName, bytes calldata _storkData)
         external
     {
-        require(dataType[_name] == 0, "Type already exists");
+        require(phalanxExists[_storkName] == false, "Type already exists");
 
-        dataType[_name] = dataTypeCount;
+        phalanxInfo[_storkName].storkTypeId = storkTypeCount;
 
-        emit StorkType(msg.sender, dataTypeCount, _name, _data);
-        dataTypeCount++;
+        emit NewStorkType(msg.sender, storkTypeCount, _storkName, _storkData);
+        phalanxExists[_storkName] == true;
+        storkTypeCount++;
+    }
+
+    /// @notice Stores new data in the StorkNet
+    /// @dev Increments the phalanx's storkLastId, makes a stork with the new id and data, then emits a event
+    /// @param _phalanxName The StorkDataType
+    /// @param _storkData The value of the data being stored
+    function createData(string memory _phalanxName, bytes memory _storkData) internal {
+        uint32 id = phalanxInfo[_phalanxName].storkLastId++;
+
+        emit StorkCreate(_phalanxName, id, Stork({_id: id, _data: _storkData}));
     }
 
     /// @notice Stores the StorkDataType in the StorkNet
     /// @dev The event emitted tells StorkNet about the data being stored, it's type, and the contract associated
-    /// @param _type The StorkDataType
-    /// @param _data The value of the data being stored
-    function storeData(string memory _type, bytes memory _data) internal {
-        emit StorkStore(msg.sender, _type, _data);
+    /// @param _phalanxName The StorkDataType
+    /// @param _storkId The value of the data being stored
+    /// @param _storkData The value of the data being stored
+    function updateData(string memory _phalanxName, uint32 _storkId, bytes memory _storkData) internal {
+        emit StorkUpdate(_phalanxName, _storkId, Stork({_id: _storkId, _data: _storkData}));
     }
+
+    /// @notice Stores the StorkDataType in the StorkNet
+    /// @dev The event emitted tells StorkNet about the data being stored, it's type, and the contract associated
+    /// @param _phalanxName The StorkDataType
+    /// @param _storkId The value of the data being stored
+    function deleteData(string memory _phalanxName, uint32 _storkId) internal {
+        emit StorkDelete(_phalanxName, _storkId);
+    }
+
 
     /// @notice Lets StorkNet know that a new data type has been created for this contract
     /// @dev This is so that we don't need to store the data type in this contract as they take a lot of space hence gas
     /// @param _from The address of the contract that created the new StorkDataType
-    /// @param _typeId The id of the created StorkDataType
-    /// @param _typeName The data type name keccak256-ed because that's how events work
-    /// @param _data The bytes version of the StorkDataType
-    event StorkType(
+    /// @param _storkTypeCount The id of the created StorkDataType
+    /// @param _storkName The data type name keccak256-ed because that's how events work
+    /// @param _storkData The bytes version of the StorkDataType
+    event NewStorkType(
         address indexed _from,
-        uint256 indexed _typeId,
-        string indexed _typeName,
-        bytes _data
+        uint256 indexed _storkTypeCount,
+        string indexed _storkName,
+        bytes _storkData
     );
 
     /// @notice Lets StorkNet know that this contract has a new Store request
-    /// @param _from The address of the contract that created the new StorkDataType
-    /// @param _typeName The data type name keccak256-ed because that's how events work
-    /// @param _data The data being stored
-    event StorkStore(
-        address indexed _from,
-        string indexed _typeName,
-        bytes _data
+    /// @param _phalanxName The address of the contract that created the new StorkDataType
+    /// @param _storkId The data type name keccak256-ed because that's how events work
+    /// @param _stork The data being stored
+    event StorkCreate(
+        string indexed _phalanxName,
+        uint32 indexed _storkId,
+        Stork _stork
+    );
+
+    /// @notice Lets StorkNet know that this contract has a new Store request
+    /// @param _phalanxName The address of the contract that created the new StorkDataType
+    /// @param _storkId The data type name keccak256-ed because that's how events work
+    /// @param _stork The data being stored
+    event StorkUpdate(
+        string indexed _phalanxName,
+        uint32 indexed _storkId,
+        Stork _stork
+    );
+
+    /// @notice Lets StorkNet know that this contract has a new Store request
+    /// @param _storkName The data type name keccak256-ed because that's how events work
+    /// @param _storkId The data being stored
+    event StorkDelete(
+        string indexed _storkName,
+        uint32 indexed _storkId
     );
 }
